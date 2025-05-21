4a9455f591697dfe67aa67f0e01740b3b75c1e66
gakki-san <gakki.nkk.0331@gmail.com>
2025-05-21 10:55:17 +0900

    refactor: 💡 refactor atmic reach voice

--- src/logic/attemptReach/index.ts
+++ src/logic/attemptReach/index.ts
@@ -6,15 +6,11 @@ export const playReachAudio = (
   setIsPopupClose: () => void,
   setReachFlags: (updater: (prev: ReachFlagsProps) => ReachFlagsProps) => void,
 ) => {
-  const audio = new Audio("/audio.mp3");
+  // const audio = new Audio("/audio.mp3");
+  const audio =
+    player !== 1 ? new Audio("/audio.mp3") : new Audio("/atmic.mp3");
 
-  if (player !== 1) {
-    audio.play();
-  } else {
-    setTimeout(() => {
-      setIsPopupClose();
-    }, 6000);
-  }
+  audio.play();
 
   audio.addEventListener("ended", () => {
     setIsPopupClose();
