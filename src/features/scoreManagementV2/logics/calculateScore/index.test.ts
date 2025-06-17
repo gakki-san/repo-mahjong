f1265de2d655edac8ff1b70c86f2b53feddbc4db
gakki-san <gakki.nkk.0331@gmail.com>
2025-06-17 22:30:11 +0900

    feat: 🎸 create test for winPoint function

--- src/features/scoreManagementV2/logics/calculateScore/index.test.ts
+++ src/features/scoreManagementV2/logics/calculateScore/index.test.ts
@@ -1,23 +1,24 @@
 import { describe, expect, it } from "vitest";
-import { calculateWinnerScore } from "@/features/scoreManagementV2/logics/calculateWinnerScore/index.ts";
+import { calculateScore } from "@/features/scoreManagementV2/logics/calculateScore/index.ts";
 import { ScoreMap } from "@/features/scoreManagementV2/hooks/useScore.ts";
 
 describe("calculateWinnerScore", () => {
+  const parent = 1;
   it("和了点数がscoreに反映される", () => {
     const winner = 0;
     const score = [25000, 25000, 25000, 25000] as ScoreMap;
     const point = 8000;
-    expect(calculateWinnerScore(winner, score, point)).toEqual([
-      33000, 25000, 25000, 25000,
+    expect(calculateScore(winner, score, point, parent)).toEqual([
+      33000, 21000, 23000, 23000,
     ]);
   });
 
-  it("和了者1のみ点数が加算される", () => {
+  it("和了者と親が同じ", () => {
     const winner = 1;
     const score = [21000, 28000, 23000, 28000] as ScoreMap;
     const point = 6000;
-    expect(calculateWinnerScore(winner, score, point)).toEqual([
-      21000, 34000, 23000, 28000,
+    expect(calculateScore(winner, score, point, parent)).toEqual([
+      19000, 34000, 21000, 26000,
     ]);
   });
 
@@ -25,17 +26,17 @@ describe("calculateWinnerScore", () => {
     const winner = 2;
     const score = [26000, 24000, 18000, 32000] as ScoreMap;
     const point = 10000;
-    expect(calculateWinnerScore(winner, score, point)).toEqual([
-      26000, 24000, 28000, 32000,
+    expect(calculateScore(winner, score, point, parent)).toEqual([
+      23500, 19000, 28000, 29500,
     ]);
   });
 
   it("和了者3のみ点数が加算される", () => {
     const winner = 3;
     const score = [30000, 20000, 25000, 25000] as ScoreMap;
-    const point = 5000;
-    expect(calculateWinnerScore(winner, score, point)).toEqual([
-      30000, 20000, 25000, 30000,
+    const point = 12000;
+    expect(calculateScore(winner, score, point, parent)).toEqual([
+      27000, 14000, 22000, 37000,
     ]);
   });
 });
